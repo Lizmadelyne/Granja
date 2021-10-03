66e3ea2f70ef58f6928e4dffd704647a39e6a255
Lizmadelyne <lizfajardop@gmail.com>
2021-10-03 07:53:58 -0500

    crudPersona

--- GAvicola/GAvicola.App/GAvicola.App.Consola/Program.cs
+++ GAvicola/GAvicola.App/GAvicola.App.Consola/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using GAvicola.App.Dominio;
-
+using GAvicola.App.Persistencia;
 namespace GAvicola.App.Consola
 {
     class Program
@@ -8,6 +9,11 @@ namespace GAvicola.App.Consola
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World EntityFramework!");
+            
+        private static IRepositorioPaciente _repoPaciente = new RepositorioPaciente();
+        private static IRepositorioMedico _repoMedico = new RepositorioMedico();
+        private static IRepositorioSignoVital _repoSignoVital = new RepositorioSignoVital();
+        static void Main(string[] args)
         }
     }
 }
