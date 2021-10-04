2d6528dcde724c45895c87ad2954f57030d9c40e
Lizmadelyne <lizfajardop@gmail.com>
2021-10-04 14:05:34 -0500

    errores page galpones

--- GAvicola/GAvicola.App/GAvicola.App.Frontend/Pages/Galpones/index.cshtml.cs
+++ GAvicola/GAvicola.App/GAvicola.App.Frontend/Pages/Galpones/index.cshtml.cs
@@ -4,16 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using GAvicola.App.Dominio;
+using GAvicola.App.Frontend;
 using GAvicola.App.Persistencia;
+using GAvicola.Dominio;
+
 
 namespace GAvicola.App.Frontend.Pages.Galpones
 {
-    public class indexModel : PageModel
+    public class IndexModel : PageModel
     {
-        private readonly IRepositorioGalpon_repoGalpon;
+        private readonly _repoGalpon;
+        public IEnumerable<Galpon> galpones { get; set; }
+        public IndexModel(IRepositorioGalpon repoGalpon)
+        {
+            _repoGalpon = repoGalpon;
+        }
         public void OnGet()
         {
+            galpones = _repoGalpon.GetAllGalpones();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is indexModel model &&
+                   EqualityComparer<IRepositorioGalpon>.Default.Equals(_repoGalpon, model._repoGalpon);
         }
     }
 }
--- GAvicola/GAvicola.App/GAvicola.App.Frontend/Startup.cs
+++ GAvicola/GAvicola.App/GAvicola.App.Frontend/Startup.cs
@@ -24,7 +24,9 @@ namespace GAvicola.App.Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+           
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
